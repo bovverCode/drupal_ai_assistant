7d5ee07f9b3b20db021698fba1ce14b047c1f79d
Sobkevych <denys.sobkevych@giftcardsgroup.com>
2026-04-30 11:44:10 +0300

    list command

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello, World! from TypeScript');
+import  { Command } from "commander";
+import { readdir } from "node:fs/promises";
+
+const program: Command = new Command();
+program.name('druppy');
+
+// List current folder command.
+program
+    .command("list")
+    .action(async () => {
+        try {
+            const files: string[] = await readdir(process.cwd());
+
+            files.forEach(file => {
+                console.log(file);
+            })
+        } catch (error) {
+            console.error(error);
+        }
+    });
+
+program.parse();
